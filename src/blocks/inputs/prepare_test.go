876c44e32d20842bb61806da75e4abae96d87454
Sweta Shaw <swt.shw94@gmail.com>
2024-12-20 14:56:21 +0530

    🎨 test: format the prepare tests

--- src/blocks/inputs/prepare_test.go
+++ src/blocks/inputs/prepare_test.go
@@ -4,10 +4,6 @@ import (
 	"context"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	gethtypes "github.com/ethereum/go-ethereum/core/types"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,151 +11,96 @@ const (
 	testDataPath = "testdata/21372637.json"
 )
 
-func TestPreparer_prepare(t *testing.T) {
-	// load test data
-	testBlock := testLoadExecInputs(t, testDataPath)
+type preparerTest struct {
+	name string
+	test func(p *preparer, ctx *preparerContext, inputs *HeavyProverInputs) error
+}
 
-	tests := []struct {
-		name    string
-		inputs  *HeavyProverInputs
-		wantErr bool
-	}{
+func setupPreparerContext(t *testing.T, p *preparer, inputs *HeavyProverInputs) *preparerContext {
+	t.Helper()
+	ctx, err := p.prepareContext(context.Background(), inputs)
+	require.NoError(t, err)
+	require.NotNil(t, ctx)
+	return ctx
+}
+
+func TestPreparer(t *testing.T) {
+	tests := []preparerTest{
 		{
-			name: "successful preparation",
-			inputs: &HeavyProverInputs{
-				ChainConfig:     testBlock.ChainConfig,
-				Block:           testBlock.Block,
-				Ancestors:       testBlock.Ancestors,
-				PreStateProofs:  testBlock.PreStateProofs,
-				PostStateProofs: testBlock.PostStateProofs,
-				Codes:           testBlock.Codes,
+			name: "prepare",
+			test: func(p *preparer, ctx *preparerContext, inputs *HeavyProverInputs) error {
+				_, err := p.prepare(ctx.ctx, inputs)
+				return err
 			},
-			wantErr: false,
 		},
 		{
-			name: "incorrect ancestors",
-			inputs: &HeavyProverInputs{
-				ChainConfig: testBlock.ChainConfig,
-				Block:       testBlock.Block,
-				Ancestors: []*gethtypes.Header{
-					{
-						Number:     testBlock.Block.Number.ToInt(),
-						ParentHash: common.Hash{},
-						Root:       common.HexToHash("0x"),
-					},
-				},
-				PreStateProofs:  testBlock.PreStateProofs,
-				PostStateProofs: testBlock.PostStateProofs,
-				Codes:           []hexutil.Bytes{},
+			name: "prepareContext",
+			test: func(p *preparer, ctx *preparerContext, inputs *HeavyProverInputs) error {
+				_, err := p.prepareContext(ctx.ctx, inputs)
+				return err
 			},
-			wantErr: true,
+		},
+		{
+			name: "preparePreState",
+			test: func(p *preparer, ctx *preparerContext, inputs *HeavyProverInputs) error {
+				return p.preparePreState(ctx, inputs)
+			},
+		},
+		{
+			name: "prepareExecParams",
+			test: func(p *preparer, ctx *preparerContext, inputs *HeavyProverInputs) error {
+				err := p.preparePreState(ctx, inputs)
+				if err != nil {
+					return err
+				}
+				_, err = p.prepareExecParams(ctx, inputs)
+				return err
+			},
+		},
+		{
+			name: "execute",
+			test: func(p *preparer, ctx *preparerContext, inputs *HeavyProverInputs) error {
+				err := p.preparePreState(ctx, inputs)
+				if err != nil {
+					return err
+				}
+				execParams, err := p.prepareExecParams(ctx, inputs)
+				if err != nil {
+					return err
+				}
+				err = p.execute(ctx, execParams)
+				return err
+			},
+		},
+		{
+			name: "prepareProverInputs",
+			test: func(p *preparer, ctx *preparerContext, inputs *HeavyProverInputs) error {
+				err := p.preparePreState(ctx, inputs)
+				if err != nil {
+					return err
+				}
+				execParams, err := p.prepareExecParams(ctx, inputs)
+				if err != nil {
+					return err
+				}
+				err = p.execute(ctx, execParams)
+				if err != nil {
+					return err
+				}
+				result := p.prepareProverInputs(ctx, execParams)
+				require.NotNil(t, result)
+				return nil
+			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			p := NewPreparer()
-			result, err := p.Prepare(context.Background(), tt.inputs)
-			if tt.wantErr {
-				assert.Error(t, err)
-				assert.Nil(t, result)
-				return
-			}
-
+			testBlock := testLoadExecInputs(t, testDataPath)
+			p := NewPreparer().(*preparer)
+			ctx := setupPreparerContext(t, p, testBlock)
+			err := tt.test(p, ctx, testBlock)
 			require.NoError(t, err)
-			require.NotNil(t, result)
-
-			// verify the output matches expected values
-			assert.Equal(t, tt.inputs.Block.Number.ToInt(), result.Block.Number.ToInt())
-			assert.Equal(t, tt.inputs.Block.ParentHash, result.Block.ParentHash)
-			assert.Equal(t, tt.inputs.Block.Root, result.Block.Root)
-			assert.Equal(t, tt.inputs.ChainConfig, result.ChainConfig)
-
-			// verify access list is initialized
-			assert.NotNil(t, result.AccessList)
-
 		})
 	}
-}
-
-func TestPreparer_prepareContext(t *testing.T) {
-	p := NewPreparer()
-	testBlock := testLoadExecInputs(t, testDataPath)
-
-	ctx, err := p.(*preparer).prepareContext(context.Background(), testBlock)
-	require.NoError(t, err)
-	require.NotNil(t, ctx)
-
-	assert.NotNil(t, ctx.trackers)
-	assert.NotNil(t, ctx.stateDB)
-	assert.NotNil(t, ctx.hc)
-}
-
-func TestPreparer_PreparePreState(t *testing.T) {
-	p := NewPreparer()
-	testBlock := testLoadExecInputs(t, testDataPath)
-
-	ctx, err := p.(*preparer).prepareContext(context.Background(), testBlock)
-	require.NoError(t, err)
-	require.NotNil(t, ctx)
-
-	err = p.(*preparer).preparePreState(ctx, testBlock)
-	require.NoError(t, err)
-}
-
-func TestPreparer_prepareExecParams(t *testing.T) {
-	p := NewPreparer()
-	testBlock := testLoadExecInputs(t, testDataPath)
-
-	ctx, err := p.(*preparer).prepareContext(context.Background(), testBlock)
-	require.NoError(t, err)
-	require.NotNil(t, ctx)
-
-	err = p.(*preparer).preparePreState(ctx, testBlock)
-	require.NoError(t, err)
-
-	execParams, err := p.(*preparer).prepareExecParams(ctx, testBlock)
-	require.NoError(t, err)
-	require.NotNil(t, execParams)
-}
-
-func TestPreparer_execute(t *testing.T) {
-	p := NewPreparer()
-	testBlock := testLoadExecInputs(t, testDataPath)
-
-	ctx, err := p.(*preparer).prepareContext(context.Background(), testBlock)
-	require.NoError(t, err)
-	require.NotNil(t, ctx)
-
-	err = p.(*preparer).preparePreState(ctx, testBlock)
-	require.NoError(t, err)
-
-	execParams, err := p.(*preparer).prepareExecParams(ctx, testBlock)
-	require.NoError(t, err)
-	require.NotNil(t, execParams)
-
-	err = p.(*preparer).execute(ctx, execParams)
-	require.NoError(t, err)
-}
-
-func TestPreparer_prepareProverInputs(t *testing.T) {
-	p := NewPreparer()
-	testBlock := testLoadExecInputs(t, testDataPath)
-
-	ctx, err := p.(*preparer).prepareContext(context.Background(), testBlock)
-	require.NoError(t, err)
-	require.NotNil(t, ctx)
-
-	err = p.(*preparer).preparePreState(ctx, testBlock)
-	require.NoError(t, err)
-
-	execParams, err := p.(*preparer).prepareExecParams(ctx, testBlock)
-	require.NoError(t, err)
-	require.NotNil(t, execParams)
-
-	err = p.(*preparer).execute(ctx, execParams)
-	require.NoError(t, err)
-
-	proverInputs := p.(*preparer).prepareProverInputs(ctx, execParams)
-	require.NotNil(t, proverInputs)
 }
