40af435f4ad1d731c75c9c1ffbb3d5679c382e77
Nicolas Maurice <nicolas.maurice.valera@gmail.com>
2025-02-14 14:21:26 +0100

    🏷️ Update Prover Input format

--- src/prover-input/input.go
+++ src/prover-input/input.go
@@ -1,7 +1,6 @@
 package input
 
 import (
-	gethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	gethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/params"
@@ -12,12 +11,35 @@ import (
 // ProverInput contains the data expected by an EVM prover engine to execute & prove the block.
 // It contains the minimal partial state & chain data necessary for processing the block and validating the final state.
 type ProverInput struct {
-	Block       *ethrpc.Block                          `json:"block"`       // Block to execute
-	Ancestors   []*gethtypes.Header                    `json:"ancestors"`   // Ancestors of the block that are accessed during the block execution
-	ChainConfig *params.ChainConfig                    `json:"chainConfig"` // Chain configuration
-	Codes       []hexutil.Bytes                        `json:"codes"`       // Contract bytecodes used during the block execution
-	PreState    []hexutil.Bytes                        `json:"preState"`    // Partial pre-state, consisting in a list of MPT nodes
-	AccessList  map[gethcommon.Address][]hexutil.Bytes `json:"accessList"`  // Access list of accounts and storage slots accessed during the block processing
+	Version     string              `json:"version"`     // Prover Input version
+	Blocks      []*Block            `json:"blocks"`      // Block to execute
+	Witness     *Witness            `json:"witness"`     // Ancestors of the block that are accessed during the block execution
+	ChainConfig *params.ChainConfig `json:"chainConfig"` // Chain configuration
+}
+
+type Witness struct {
+	State     []hexutil.Bytes     `json:"state"`     // Partial pre-state, consisting in a list of MPT nodes
+	Ancestors []*gethtypes.Header `json:"ancestors"` // Ancestors of the block that are accessed during the block execution
+	Codes     []hexutil.Bytes     `json:"codes"`     // Contract bytecodes used during the block execution
+}
+
+type Block struct {
+	Header       *gethtypes.Header        `json:"header"`
+	Transactions []*gethtypes.Transaction `json:"transaction"`
+	Uncles       []*gethtypes.Header      `json:"uncles"`
+	Withdrawals  []*gethtypes.Withdrawal  `json:"withdrawals"`
+}
+
+func (b *Block) Block() *gethtypes.Block {
+	return gethtypes.
+		NewBlockWithHeader(b.Header).
+		WithBody(
+			gethtypes.Body{
+				Transactions: b.Transactions,
+				Uncles:       b.Uncles,
+				Withdrawals:  b.Withdrawals,
+			},
+		)
 }
 
 // PreflightData contains data expected by an EVM prover engine to execute & prove the block.
