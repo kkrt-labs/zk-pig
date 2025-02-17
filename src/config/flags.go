ec6bc3cd89d25a05fcb428d9fabae3050bde2eaa
Sweta Shaw <swt.shw94@gmail.com>
2025-02-17 16:28:17 +0530

    Revert "🐛 Fix CLI flgas for chain"
    This reverts commit 49e5cad96d13440360e2cbad71a936935ffb651f.

--- src/config/flags.go
+++ src/config/flags.go
@@ -128,6 +128,7 @@ func AddStoreFlags(v *viper.Viper, f *pflag.FlagSet) {
 }
 
 func AddProverInputFlags(v *viper.Viper, f *pflag.FlagSet) {
+	AddChainFlags(v, f)
 	AddAWSFlags(v, f)
 	AddChainFlags(v, f)
 	AddStoreFlags(v, f)
